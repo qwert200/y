448887c0d5ac54becfe2d45a54c5625f85370728
TheTeamInsane <115443754+TheTeamInsane@users.noreply.github.com>
2022-11-24 16:19:27 +0530

    Update dev.py

--- InsaneMusic/plugins/devs/dev.py
+++ InsaneMusic/plugins/devs/dev.py
@@ -14,7 +14,7 @@ from time import time
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
+from config import OWNER_ID
 from InsaneMusic import app
 
 
@@ -32,7 +32,7 @@ async def edit_or_reply(msg: Message, **kwargs):
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 
-@app.on_message(filters.command("eval") & None & ~filters.forwarded & ~filters.via_bot)
+@app.on_message(filters.command("eval") & filters.command("OWNER_ID") & ~filters.forwarded & ~filters.via_bot)
 async def executor(client, message):
     if len(message.command) < 2:
         return await edit_or_reply(
@@ -133,7 +133,7 @@ async def forceclose_command(_, CallbackQuery):
         return
 
 
-@app.on_message(filters.command("sh") & None & ~filters.forwarded & ~filters.via_bot)
+@app.on_message(filters.command("sh") & filters.command("OWNER_ID") & ~filters.forwarded & ~filters.via_bot)
 async def shellrunner(client, message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="**Usage:**\n/sh git pull")
